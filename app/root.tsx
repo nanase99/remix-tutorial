79704af790cdd5889e2544314fa40a78b667bb7b
nanase <sgmdq601@gmail.com>
2024-08-17 12:43:38 +0900

    refactor: format

--- app/root.tsx
+++ app/root.tsx
@@ -36,10 +36,10 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <a href={"/contacts/1"}>Your Name</a>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <a href={"/contacts/2"}>Your Friend</a>
               </li>
             </ul>
           </nav>
